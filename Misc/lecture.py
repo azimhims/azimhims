07bc2ad6c199e64c35b96822ae12a1ace4e7b287
azimhims <136650936+azimhims@users.noreply.github.com>
2024-07-08 23:08:44 +0500

    Using SQLModel to store record into Database only

---
+++ Misc/lecture.py
@@ -0,0 +1,32 @@
+#        '''import''' 
+#1.Define models 	===>pydantic
+#2.Create engine 	===>sqlalchamy
+#3.Create table 	===>sqlalchamy
+#4.Insert record 	===>sqlalchamy
+#5.Query data 		===>sqlalchamy
+#6.Update data 		===>sqlalchamy
+#7.Delete data 		===>sqlalchamy
+#8.Commit 		===>sqlalchamy    save in database
+#9.Close sessin		===>sqlalchamy
+
+from sqlmodel import SQLModel, Field, create_engine, Session
+from typing import Optional
+class lecture(SQLModel, table=True):
+        id: Optional[int]= Field(default=None, primary_key=True)
+        subject: str
+        teacher: str
+
+lect1 = lecture(subject= 'OOPS', teacher='junaid')
+lect2 = lecture(subject="ML", techer='qasim')
+
+DATABAE_URL = "postgresql://testdb_owner:Gxf49mATunVW@ep-shy-cake-a1jkjuiw.ap-southeast-1.aws.neon.tech/testdb?sslmode=require"
+engine = create_engine(DATABAE_URL)
+
+SQLModel.metadata.create_all(engine) #table creating
+
+
+with Session(engine) as session:
+        session.add(lect1)
+        session.commit()
+        session.close()
+        
